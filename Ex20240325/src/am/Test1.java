31db079510c19844d871a6702a9b39bc0af1d039
wjsxorjs <wjsxorjs@naver.com>
2024-03-25 11:51:37 +0900

    JAVA 2024.03.25 Ex 1

---
+++ Ex20240325/src/am/Test1.java
@@ -0,0 +1,14 @@
+package am;
+
+public class Test1 {
+	
+	/*JAVA에서 main함수가 시작하는 부분이다.
+	(main함수는 JVM에 의해 자동 호출되는 곳이다.)*/
+	public static void main(String[] args) { //영역 시작
+		System.out.println("Hello, World!");
+		System.out.println("Hello, JAVA!");
+		System.out.println("World, JAVA!");
+		System.out.println("JAVA, World!");
+	} // 영역 끝
+	
+}
