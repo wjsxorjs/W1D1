55a2c65d37cd48c98260e5370c1c36302561a580
wjsxorjs <wjsxorjs@naver.com>
2024-03-25 12:01:15 +0900

    JAVA 2024.03.25 Ex 1 .3

--- Ex20240325/src/am/Test1.java
+++ Ex20240325/src/am/Test1.java
@@ -1,10 +1,20 @@
 package am;
 
+/**
+ * 
+ * @author Jack Esther
+ *
+ */
+
 public class Test1 {
 	
-	/*JAVA에서 main함수가 시작하는 부분이다.
-	(main함수는 JVM에 의해 자동 호출되는 곳이다.)*/
-	public static void main(String[] args) { //main함수의 경우 변수명을 제외한 모든 건 수정불가하다.  //영역 시작
+	/*
+	 JAVA에서 main함수가 시작하는 부분이다.
+	 (main함수는 JVM에 의해 자동 호출되는 곳이다.)
+	 main함수의 경우 변수명을 제외한 public static void main(String[]은 절대 수정이 불가하다.  
+	*/
+	
+	public static void main(String[] args) { //영역 시작
 		System.out.println("Hello, World!");
 		System.out.println("Hello, JAVA!");
 		System.out.println("World, JAVA!");
